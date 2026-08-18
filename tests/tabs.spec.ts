c698cb84e7e6b649cb6e59dd7b47119b32af6c54
Prince <prince@Princes-MacBook-Air.local>
2026-08-19 03:04:33 +0530

    tabs tests added

---
+++ tests/tabs.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+import { TestConfig } from '../test.config';
+import { Tabs } from '../pages/Tabs';
+
+let testConfig : TestConfig;
+test.beforeEach(async()=>{
+  testConfig = new TestConfig();
+})
+
+test('verifing new tab', async ({ page }) => {
+
+  let tabs = new Tabs(page);
+  await page.goto(testConfig.appUrl);
+  const newTabTitle = await tabs.clickTabButton();
+  expect(newTabTitle).toContain('SDET');
+  
+});
+
+test('verifing Pop Ups', async ({ context }) => {
+
+  const page = await context.newPage()
+  let tabs = new Tabs(page);
+  await page.goto(testConfig.appUrl);
+  await tabs.clickPopUpButton();
+  const pages = context.pages();
+  for(let pw of pages.slice(1)){
+    await pw.close();
+  }
+  expect(context.pages()).toHaveLength(1);
+  
+});
